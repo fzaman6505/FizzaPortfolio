72184438f66ab876abaf4bf74097f05d451a1693
Fizza Zaman <fzaman6505@gmail.com>
2019-06-19 23:30:09 -0700

    Added portfolio items via resource generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  resources :portfolios
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
-  get 'pages/Home'
-  get 'pages/About'
-  get 'pages/Contact'
+  
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
